c425ae4497c95c9a9b68c1b8db6c71453bc2f572
Ponakala-RupaRani <srinivasuluponakala9@gmail.com>
2026-08-14 16:47:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,4 @@
-import streamlit as st
-
-st.set_page_config(
-    page_title="Adaptive Traffic Signal Control System",
-    page_icon="🚦",
-    layout="centered"
-)
+# ================= LOGIN PAGE =================
 
 if "logged_in" not in st.session_state:
     st.session_state.logged_in = False
@@ -14,31 +8,19 @@ if not st.session_state.logged_in:
     st.markdown(
         """
         <div style="
-            text-align:center;
-            padding:40px 10px 20px 10px;
+            text-align: center;
+            margin-top: 80px;
+            margin-bottom: 30px;
         ">
-
             <div style="
-                font-size:75px;
+                font-size: 90px;
             ">
                 🚦
             </div>
-
-            <h1>
-                ADAPTIVE TRAFFIC SIGNAL
-                CONTROL SYSTEM
-            </h1>
-
-            <p style="font-size:18px;">
-                AI-Based Four-Way Traffic Management System
-            </p>
-
         </div>
         """,
         unsafe_allow_html=True
     )
-
-    st.markdown("---")
 
     username = st.text_input(
         "Username",
@@ -51,10 +33,12 @@ if not st.session_state.logged_in:
         placeholder="Enter password"
     )
 
-    if st.button(
-        "🚦 LOGIN",
+    login = st.button(
+        "LOGIN",
         use_container_width=True
-    ):
+    )
+
+    if login:
 
         if username == "admin" and password == "traffic123":
 
@@ -63,8 +47,6 @@ if not st.session_state.logged_in:
 
         else:
 
-            st.error(
-                "❌ Invalid username or password"
-            )
+            st.error("Invalid username or password")
 
     st.stop()
